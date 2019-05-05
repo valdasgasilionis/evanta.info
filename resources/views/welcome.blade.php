f907b5b306d7261d52527a27d19b7b41f0b3ba56
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-05-05 15:18:48 +0300

    ag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    2 kambarių butas nuomai VANAGUPĖJE
                 </div>
 
                 <div class="links">
